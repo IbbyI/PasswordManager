74d39f37e37bf9d9917a06632da386602a03c864
Valorrr <ibbyissa001@gmail.com>
2025-02-19 19:13:30 +0000

    PEP8 Format & Added Logging

--- handlers/encryption_manager.py
+++ handlers/encryption_manager.py
@@ -1,6 +1,7 @@
-import os
 import hashlib
+import os
 from typing import Any, Optional
+
 from cryptography.fernet import Fernet
 
 
@@ -15,15 +16,12 @@ class EncryptionManager:
         """
         key = os.getenv("FERNET_KEY")
         if not key:
-            self.log_manager.write_log(
-                error_message="Encryption key is missing or invalid."
-            )
+            self.log_manager.log("Warning", "Fernet Encryption Key Could Not Be Found.")
             return None
         try:
             return key.encode()
-        except Exception as e:
-            self.log_manager.write_log(
-                error_message=f"Failed to encode key: {e}")
+        except Exception as error:
+            self.log_manager.log("Error", f"Failed to encode key: {error}")
             return None
 
     def encrypt(self, data: list) -> list[Any]:
@@ -31,22 +29,26 @@ class EncryptionManager:
         Encrypts Given Data using Fernet Encryption
         """
         if self.key is None:
-            self.log_manager.write_log(
-                error_message=f"Encryption key is missing or invalid.")
+            self.log_manager.log("Error", f"Encryption key is missing or invalid.")
             raise ValueError("Encryption key is missing or invalid.")
         cipher = Fernet(self.key)
-        return [cipher.encrypt(item.encode()) if isinstance(item, str) else item for item in data]
+        return [
+            cipher.encrypt(item.encode()) if isinstance(item, str) else item
+            for item in data
+        ]
 
     def decrypt(self, data: list) -> list[Any]:
         """
         Decrypts Fernet Encrypted Data
         """
         if self.key is None:
-            self.log_manager.write_log(
-                error_message=f"Encryption key is missing or invalid.")
+            self.log_manager.log("Error", "Encryption key is missing or invalid.")
             raise ValueError("Encryption key is missing or invalid.")
         cipher = Fernet(self.key)
-        return [cipher.decrypt(item).decode() if isinstance(item, bytes) else item for item in data]
+        return [
+            cipher.decrypt(item).decode() if isinstance(item, bytes) else item
+            for item in data
+        ]
 
     def generate_salt(self) -> bytes:
         """
@@ -54,13 +56,8 @@ class EncryptionManager:
         """
         return os.urandom(32)
 
-    def hash_password(
-        self,
-        password: str,
-        salt: bytes,
-        iterations=10000
-    ) -> bytes:
+    def hash_password(self, password: str, salt: bytes, iterations=10000) -> bytes:
         """
         Hashes Given Password
         """
-        return hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, iterations)
+        return hashlib.pbkdf2_hmac("sha256", password.encode("utf-8"), salt, iterations)
