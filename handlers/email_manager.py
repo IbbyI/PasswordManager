783d4a9fdf1fb7d640e6ac20a522179396485b3f
Valorrr <ibbyissa001@gmail.com>
2025-02-19 19:12:25 +0000

    Modular Email Sending, Better Type-Checking

--- handlers/email_manager.py
+++ handlers/email_manager.py
@@ -1,9 +1,14 @@
 import os
 import re
-import requests
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from string import Template
+from threading import Thread
+from typing import Optional
+
+import requests
+from premailer import Premailer
 
 
 class EmailManager:
@@ -15,38 +20,63 @@ class EmailManager:
         self.smtp_server = "smtp.gmail.com"
         self.smtp_port = 465
 
-    def send_email(self, recipient_email: str, file: str) -> None:
+    def send_email(
+        self,
+        recipient_email: str,
+        file_path: str,
+        otp: Optional[int] = None,
+        number_of_accounts: Optional[int] = None,
+    ) -> None:
         """
-        Sends email to recipient email using SMTP.
+        Sends an email to the recipient using SMTP.
         """
         try:
             sender_email = os.environ.get("email", "")
             sender_password = os.environ.get("appsPassword", "")
+
             message = MIMEMultipart("alternative")
             message["Subject"] = "Password Manager"
             message["From"] = sender_email
             message["To"] = recipient_email
 
-            with open(file, "r", encoding="utf-8") as f:
+            with open(file_path, "r", encoding="utf-8") as f:
                 html_content = f.read()
 
-            html_text = MIMEText(html_content, "html")
+            html_content_inlined = Premailer(
+                html_content, remove_classes=True
+            ).transform()
+
+            template = Template(html_content_inlined)
+            substitutions = {"user": recipient_email.split("@")[0]}
+            if otp:
+                substitutions["otp"] = str(otp)
+            if number_of_accounts:
+                substitutions["number_of_accounts"] = str(number_of_accounts)
+
+            formatted_html = template.safe_substitute(substitutions)
+            html_text = MIMEText((formatted_html), "html")
             message.attach(html_text)
 
             with smtplib.SMTP_SSL(self.smtp_server, self.smtp_port) as server:
                 server.login(sender_email, sender_password)
-                server.sendmail(sender_email, recipient_email,
-                                message.as_string())
+                server.sendmail(sender_email, recipient_email, message.as_string())
+
             print("Email sent successfully.")
-        except smtplib.SMTPResponseException as e:
-            self.log_manager.write_log(error_message=e)
+        except smtplib.SMTPResponseException as error:
+            self.log_manager.log("Error", f"Could Not Send Email: {error}")
+
+    def send_email_thread(self, email: str, formatted_html: str) -> None:
+        """
+        Starts a thread to send an email.
+        """
+        Thread(target=self.send_email, args=(email, formatted_html, False)).start()
 
     def is_valid_email(self, email: str) -> bool:
         """
         Checks if given email is valid using regex.
         """
         regex = re.compile(
-            r'^[a-zA-Z0-9.!#$%&` *+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
+            r"^[a-zA-Z0-9.!#$%&` *+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
         )
         if regex.match(email):
             return True
@@ -56,7 +86,9 @@ class EmailManager:
         """
         Compares given password to pwned password list.
         """
-        url = "https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.json"
+        url = (
+            "https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.json"
+        )
         response = requests.get(url)
         pass_list = set(response.json())
 
