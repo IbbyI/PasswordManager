7dd3ad56729b85dfe6ce1af9fa0ab08b2f389ff2
Valorrr <ibbyissa001@gmail.com>
2025-02-28 16:17:05 +0000

    Enhance OTPManager with detailed docstrings for class methods and parameters

--- handlers/otp_manager.py
+++ handlers/otp_manager.py
@@ -1,9 +1,22 @@
 import secrets
 from typing import Optional
 
+from handlers.email_manager import EmailManager
+from handlers.log_manager import LogManager
+
 
 class OTPManager:
-    def __init__(self, email_manager, log_manager) -> None:
+    """
+    Manages OTP generation and verification.
+    """
+
+    def __init__(self, email_manager: EmailManager, log_manager: LogManager) -> None:
+        """
+        Initialize the OTPManager with dependencies.
+        Args:
+            email_manager: The email manager object.
+            log_manager: The log manager object.
+        """
         self.email_manager = email_manager
         self.log_manager = log_manager
         self.OTP: Optional[int] = None
@@ -12,6 +25,8 @@ class OTPManager:
     def generate_otp(self) -> int:
         """
         Generates a random 6-digit number as OTP.
+        Returns:
+            int: The generated OTP.
         """
         self.OTP = secrets.randbelow(900000) + 100000
         return self.OTP
@@ -19,5 +34,9 @@ class OTPManager:
     def verify_otp(self, otp: int) -> bool:
         """
         Verifies if the provided OTP matches the generated OTP.
+        Args:
+            otp (int): The OTP to be verified.
+        Returns:
+            bool: True if the OTP matches, False otherwise.
         """
         return self.OTP == otp
