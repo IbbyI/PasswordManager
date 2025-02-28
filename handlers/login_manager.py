b144ba089bb9fb7f177fdc20aa1528516c9f531f
Valorrr <ibbyissa001@gmail.com>
2025-02-28 16:16:57 +0000

    Enhance LoginManager with detailed docstrings for methods and parameters and uses customtkiner

--- handlers/login_manager.py
+++ handlers/login_manager.py
@@ -1,33 +1,40 @@
-import tkinter as tk
-from ttkbootstrap import ttk
+import customtkinter as ctk
 from tkinter import BOTTOM, messagebox
 
-import ttkbootstrap as tb
-
+from handlers.database_manager import DatabaseManager
 from handlers.email_manager import EmailManager
 from handlers.encryption_manager import EncryptionManager
 from handlers.log_manager import LogManager
 from handlers.otp_manager import OTPManager
+from handlers.password_generator import PasswordGenerator
 
 
-class MasterLogin:
-    def __init__(self, db_manager, login_success) -> None:
+class LoginManager:
+    """
+    Manages the master login UI screen which verifies user data.
+    """
+
+    def __init__(self, database_manager: DatabaseManager, login_success) -> None:
         """
-        Manages the master login UI screen which verifies user data.
+        Initialize the LoginManager with dependencies.
+        Args:
+            database_manager: The database manager object.
+            login_success: The function to call when login is successful.
         """
-        self.db_manager = db_manager
+        self.database_manager = database_manager
         self.log_manager = LogManager()
+        self.password_generator = PasswordGenerator()
         self.encryption_manager = EncryptionManager(self.log_manager)
         self.email_manager = EmailManager(self.log_manager)
         self.otp_manager = OTPManager(self.email_manager, self.log_manager)
 
-        self.login_window = tk.Tk()
+        self.login_window = ctk.CTk()
         self.login_window.title("Password Manager")
         self.login_window.resizable(False, False)
         self.login_window.geometry("350x270+550+150")
 
-        self.style = tb.Style()
-        self.style.theme_use("cyborg")
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
 
         self.login_success = login_success
 
@@ -40,6 +47,8 @@ class MasterLogin:
     def switch_mode(self, action="signup") -> None:
         """
         Switches UI mode dynamically.
+        Args:
+            action (str): The action to perform.
         """
         if self.current_frame:
             self.current_frame.destroy()
@@ -55,12 +64,26 @@ class MasterLogin:
         elif action == "reset_password":
             self.reset_password_ui()
 
-    def create_label_entry(self, frame: ttk.Frame, label_text, show: str | None = None):
+    def create_label_entry(
+        self, frame: ctk.CTkFrame, label_text, show: str | None = None
+    ) -> ctk.CTkEntry:
         """
         Creates and returns a label and entry widget pair.
+        Args:
+            frame (ctk.CTkFrame): The frame to place the widgets in.
+            label_text (str): The text to display on the label.
+            show (str | None): The character to show in the entry widget. Defaults to None.
+        Returns:
+            ctk.CTkEntry: The entry widget created.
         """
-        ttk.Label(frame, text=label_text).pack()
-        entry = ttk.Entry(frame, show=show if show is not None else "", width=22)
+        ctk.CTkLabel(frame, text=label_text).pack()
+        entry = ctk.CTkEntry(
+            frame,
+            show=show if show is not None else "",
+            width=150,
+            bg_color="#242525",
+            fg_color="#242525",
+        )
         entry.pack(pady=3)
         return entry
 
@@ -69,7 +92,7 @@ class MasterLogin:
         Creates the sign-up UI widgets.
         """
         self.bottom_frame.forget()
-        frame = ttk.Frame(self.login_window)
+        frame = ctk.CTkFrame(self.login_window, fg_color="#242525")
         frame.focus()
         frame.pack()
         self.current_frame = frame
@@ -84,16 +107,22 @@ class MasterLogin:
             frame, "Confirm your Password:", show="*"
         )
 
-        ttk.Button(
+        ctk.CTkButton(
             frame,
             text="Register",
             command=lambda: self.validate_user(
                 email_entry, password_entry, confirm_password_entry, action="new"
             ),
         ).pack(pady=3)
-        ttk.Button(
+        ctk.CTkButton(
             frame, text="Login", command=lambda: self.switch_mode("login")
-        ).pack()
+        ).pack(side=ctk.BOTTOM)
+
+        ctk.CTkButton(
+            frame,
+            text="Generate Password",
+            command=lambda: self.password_generator.generate_window(),
+        ).pack(side=ctk.BOTTOM)
 
         frame.bind(
             "<Return>",
@@ -106,7 +135,11 @@ class MasterLogin:
         """
         Creates login UI widgets.
         """
-        frame = ttk.Frame(self.login_window)
+        frame = ctk.CTkFrame(
+            self.login_window,
+            bg_color="#242525",
+            fg_color="#242525",
+        )
         frame.focus()
         frame.pack()
         self.current_frame = frame
@@ -115,21 +148,23 @@ class MasterLogin:
 
         email_entry = self.create_label_entry(frame, "Email")
         password_entry = self.create_label_entry(frame, "Password", show="*")
-        ttk.Button(
+        email_entry.insert(0, "ibbyissa001@gmail.com")
+        password_entry.insert(0, "Password12@")
+        ctk.CTkButton(
             frame,
             text="Log In",
             command=lambda: self.credentials_check(email_entry, password_entry),
         ).pack(pady=5)
 
-        self.bottom_frame = ttk.Frame(self.login_window)
+        self.bottom_frame = ctk.CTkFrame(self.login_window, fg_color="#242525")
         self.bottom_frame.pack(side=BOTTOM, pady=10)
 
-        ttk.Button(
+        ctk.CTkButton(
             self.bottom_frame,
             text="Sign Up",
             command=lambda: self.switch_mode("signup"),
         ).pack(pady=2)
-        ttk.Button(
+        ctk.CTkButton(
             self.bottom_frame,
             text="Forgot Password",
             command=lambda: self.switch_mode("forgot_password"),
@@ -142,13 +177,18 @@ class MasterLogin:
 
     def validate_user(
         self,
-        email_entry: ttk.Entry,
-        password_entry: ttk.Entry,
-        confirm_entry: ttk.Entry,
+        email_entry: ctk.CTkEntry,
+        password_entry: ctk.CTkEntry,
+        confirm_entry: ctk.CTkEntry,
         action="new",
     ) -> None:
         """
         Validates user data.
+        Args:
+            email_entry (ctk.CTkEntry): The email entry widget.
+            password_entry (ctk.CTkEntry): The password entry widget.
+            confirm_entry (ctk.CTkEntry): The confirm password entry widget.
+            action (str): The action to perform. Defaults to "new".
         """
         email = email_entry.get()
         password = password_entry.get()
@@ -173,10 +213,13 @@ class MasterLogin:
             messagebox.showerror("Error", "Passwords do not match.")
 
     def credentials_check(
-        self, email_entry: ttk.Entry, password_entry: ttk.Entry
+        self, email_entry: ctk.CTkEntry, password_entry: ctk.CTkEntry
     ) -> None:
         """
         Validates login credentials.
+        Args:
+            email_entry (ctk.CTkEntry): The email entry widget.
+            password_entry (ctk.CTkEntry): The password entry widget.
         """
         email = email_entry.get()
         password = password_entry.get()
@@ -187,7 +230,7 @@ class MasterLogin:
             )
             return
 
-        results = self.db_manager.search_user(email)
+        results = self.database_manager.search_user(email)
         if not results:
             messagebox.showerror("Invalid Credentials", "Incorrect email or password.")
             return
@@ -197,7 +240,7 @@ class MasterLogin:
             messagebox.showerror("Invalid Credentials", "Incorrect email or password.")
             return
 
-        self.db_manager.set_user_id(user_id)
+        self.database_manager.set_user_id(user_id)
         self.login_window.withdraw()
         self.login_success()
 
@@ -206,7 +249,7 @@ class MasterLogin:
         Creates forgot password UI widgets.
         """
         self.bottom_frame.pack_forget()
-        frame = ttk.Frame(self.login_window)
+        frame = ctk.CTkFrame(self.login_window, fg_color="#242525")
         frame.focus()
         frame.pack()
         self.current_frame = frame
@@ -214,42 +257,23 @@ class MasterLogin:
         self.login_window.title("Reset Password")
         email_entry = self.create_label_entry(frame, "Enter your Email:")
 
-        ttk.Button(
+        ctk.CTkButton(
             frame,
             text="Reset Password",
             command=lambda: self.handle_reset(email_entry.get()),
         ).pack(pady=5)
-        ttk.Button(
+        ctk.CTkButton(
             frame, text="Login", command=lambda: self.switch_mode("login")
         ).pack()
 
-        frame.bind("<Return>", lambda event: self.handle_reset(email_entry.get()))
-
-    def handle_reset(self, email: str) -> None:
-        """
-        Handles OTP generation and sends via email.
-        """
-        user = self.db_manager.search_user(email)
-        self.email = email
-        if user:
-            self.user_id = user[0]
-            otp = self.otp_manager.generate_otp()
-            self.email_manager.send_email(
-                email, file_path="./templates/otp_template.html", otp=otp
-            )
-            messagebox.showinfo("OTP Sent", "Please check your email.")
-            self.switch_mode("otp_check")
-        else:
-            messagebox.showwarning(
-                "Email Not Found", "Email hasn't been registered yet."
-            )
+        frame.bind("<Return>", self.handle_reset(email_entry.get()))
 
     def otp_check_ui(self) -> None:
         """
         Creates OTP check UI widgets.
         """
         self.bottom_frame.pack_forget()
-        frame = ttk.Frame(self.login_window)
+        frame = ctk.CTkFrame(self.login_window, fg_color="#242525")
         frame.focus()
         frame.pack()
         self.current_frame = frame
@@ -257,20 +281,22 @@ class MasterLogin:
         self.login_window.title("Enter OTP")
         otp_entry = self.create_label_entry(frame, "Enter OTP:")
 
-        ttk.Button(
+        ctk.CTkButton(
             frame,
             text="Verify OTP",
             command=lambda: self.verify_user_otp(otp_entry.get()),
         ).pack(pady=5)
-        ttk.Button(
+        ctk.CTkButton(
             frame, text="Login", command=lambda: self.switch_mode("login")
         ).pack()
 
-        frame.bind("<Return>", lambda event: self.verify_user_otp(otp_entry.get()))
+        frame.bind("<Return>", self.verify_user_otp(otp_entry.get()))
 
-    def verify_user_otp(self, otp: str) -> None:
+    def verify_user_otp(self, otp: str, event=None) -> None:
         """
         Verifies the entered OTP.
+        Args:
+            otp (str): The OTP entered by the user.
         """
         if self.otp_manager.verify_otp(int(otp)):
             self.log_manager.log("Info", f"Successful OTP Check for: {self.email}")
@@ -285,7 +311,7 @@ class MasterLogin:
         Creates the password reset UI widgets.
         """
         self.bottom_frame.pack_forget()
-        frame = ttk.Frame(self.login_window)
+        frame = ctk.CTkFrame(self.login_window, fg_color="#242525")
         frame.focus()
         frame.pack()
         self.current_frame = frame
@@ -296,14 +322,20 @@ class MasterLogin:
         confirm_password_entry = self.create_label_entry(
             frame, "Confirm New Password:", show="*"
         )
-
-        ttk.Button(
+        ctk.CTkButton(
             frame,
             text="Reset Password",
             command=lambda: self.reset_password(password_entry, confirm_password_entry),
         ).pack(pady=5)
-        ttk.Button(
+
+        ctk.CTkButton(
             frame, text="Login", command=lambda: self.switch_mode("login")
+        ).pack()
+
+        ctk.CTkButton(
+            frame,
+            text="Generate Password",
+            command=lambda: self.password_generator.generate_window(),
         ).pack()
 
         frame.bind(
@@ -311,11 +343,38 @@ class MasterLogin:
             lambda event: self.reset_password(password_entry, confirm_password_entry),
         )
 
+    def handle_reset(self, email: str, event=None) -> None:
+        """
+        Handles OTP generation and sends via email.
+        Args:
+            email (str): The user's email.
+        """
+        if not email:
+            messagebox.showwarning("Email Required", "Please enter your email.")
+            return
+        user = self.database_manager.search_user(email)
+        self.email = email
+        if user:
+            self.user_id = user[0]
+            otp = self.otp_manager.generate_otp()
+            self.email_manager.send_email(
+                email, file_path="./templates/otp_template.html", otp=otp
+            )
+            messagebox.showinfo("OTP Sent", "Please check your email.")
+            self.switch_mode("otp_check")
+        else:
+            messagebox.showwarning(
+                "Email Not Found", "Email hasn't been registered yet."
+            )
+
     def reset_password(
-        self, password_entry: ttk.Entry, confirm_password_entry: ttk.Entry
+        self, password_entry: ctk.CTkEntry, confirm_password_entry: ctk.CTkEntry
     ) -> None:
         """
         Handles the password reset process.
+        Args:
+            password_entry (ctk.CTkEntry): The password entry widget.
+            confirm_password_entry (ctk.CTkEntry): The confirm password entry widget.
         """
         password = password_entry.get()
         confirm_password = confirm_password_entry.get()
@@ -324,11 +383,17 @@ class MasterLogin:
             messagebox.showerror("Error ", "Choose a stronger password.")
             return
 
+        if self.user_id is None:
+            messagebox.showerror("Error", "User is not authenticated.")
+            return
+
         if password == confirm_password:
-            email = self.db_manager.get_email(self.user_id)[0]
+            email_tuple = self.database_manager.get_email(self.user_id)
+            if email_tuple:
+                email = email_tuple[0]
             salt = self.encryption_manager.generate_salt()
             hash = self.encryption_manager.hash_password(password, salt)
-            self.db_manager.update_user(email, hash, salt)
+            self.database_manager.update_user(email, hash, salt)
             messagebox.showinfo("Success", "Password reset successfully.")
             self.switch_mode("login")
         else:
@@ -337,11 +402,14 @@ class MasterLogin:
     def new_user_handler(self, email: str, password: str) -> None:
         """
         Handles new user registration & Sends Formatted Email with User's Username.
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
         """
         salt = self.encryption_manager.generate_salt()
         hash = self.encryption_manager.hash_password(password, salt)
         try:
-            self.db_manager.create_new_user(email, hash, salt)
+            self.database_manager.create_new_user(email, hash, salt)
             self.switch_mode("login")
             self.email_manager.send_email(email, file_path="./templates/welcome.html")
 
@@ -351,10 +419,13 @@ class MasterLogin:
     def update_user_handler(self, email: str, password: str) -> None:
         """
         Handles updating user password & Sends Formatted Email with User's Username.
+        Args:
+            email (str): The user's email.
+            password (str): The user's password.
         """
         salt = self.encryption_manager.generate_salt()
         hash = self.encryption_manager.hash_password(password, salt)
-        self.db_manager.update_user(email, hash, salt)
+        self.database_manager.update_user(email, hash, salt)
         self.switch_mode("login")
         self.email_manager.send_email(
             email, file_path="./templates/update_account.html"
